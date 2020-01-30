5680b512ffbbeefa128d83371fe3d2af8c50d5f0
NascimentoLucas <nascimentol@outlook.com>
2020-01-30 00:50:50 -0300

    updated load html

--- loadHtml.js
+++ loadHtml.js
@@ -1,6 +1,6 @@
 function start(){
 	console.log("start load html js");
-	document.getElementById("content").innerHTML='<object type="text/html" data="index.html" ></object>';
+	document.getElementById("content").innerHTML='<object type="text/html" data="https://nascimentolucas.github.io" ></object>';
 }
 
 window.onload = start;
