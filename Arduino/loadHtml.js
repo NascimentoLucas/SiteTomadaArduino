b75d04caf825bd642d5f23136057fd61a21be480
NascimentoLucas <nascimentol@outlook.com>
2020-02-06 02:52:27 -0300

    updated link

--- Arduino/loadHtml.js
+++ Arduino/loadHtml.js
@@ -1,6 +1,6 @@
 function start(){
 	console.log("start load html js");
-	document.getElementById("content").innerHTML='<object type="text/html" data="https://nascimentolucas.github.io" ></object>';
+	document.getElementById("content").innerHTML='<object type="text/html" data="http://devlucasnascimento.com.br/Arduino/" ></object>';
 }
 
 window.onload = start;
