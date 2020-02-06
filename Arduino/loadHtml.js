6b9c0dad18f4d3f04f2af9c89fb0e7af069c10b4
NascimentoLucas <nascimentol@outlook.com>
2020-02-06 04:57:18 -0300

    fixed path laod html

--- Arduino/loadHtml.js
+++ Arduino/loadHtml.js
@@ -1,7 +1,7 @@
 function start(){
 	console.log("Start Load");
 	var path = "http://devlucasnascimento.com.br/Arduino/";	//todo remove
-	path = 'http://localhost:9000/Arduino/';
+	//path = 'http://localhost:9000/Arduino/';
 	var html = "<object type=\"text/html\" data=" + path +" ></object>";
 	
 	var obj = document.getElementById("content");
