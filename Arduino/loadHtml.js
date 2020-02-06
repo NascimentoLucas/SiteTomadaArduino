bee5f5a147f38cf96ac7d29fdb1bb10dff9322b7
NascimentoLucas <nascimentol@outlook.com>
2020-02-06 02:54:29 -0300

    added debug msg

--- Arduino/loadHtml.js
+++ Arduino/loadHtml.js
@@ -1,4 +1,5 @@
 function start(){
+	console.log("Hostgator");
 	console.log("start load html js");
 	document.getElementById("content").innerHTML='<object type="text/html" data="http://devlucasnascimento.com.br/Arduino/" ></object>';
 }
